0b2abeff7c1ee5fbc477fc15060dfc37040ec48f
YuraFihura <110775193+YuraFihura@users.noreply.github.com>
2022-10-06 09:27:16 +0300

    1 коміт в гілці master створено

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+﻿
+// Lab_02.cpp
+// Фігури Юрія
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 24
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+	double a;
+	double z1;
+	double z2;
+	cout << "a = "; cin >> a;
+	z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
+	//z2 = tan(3 * a);
+	z2 = sin(3*a) / cos(3*a);
+	cout << "z1 =" << z1;
+	cout << "   z2 =" << z2;
+	cin.get();
+	return 0;
+
+}
+
